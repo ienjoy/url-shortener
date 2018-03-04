a7a41dc7b196e0ede3d6f75fda3b1f727508b6be
ienjoy <ienjoy@mailinator.com>
2018-03-05 10:04:58 +1300

    Adding the ability to add new links
    Adding the ability to add new links

--- index.php
+++ index.php
@@ -1,51 +1,64 @@
 <?php
-// quick short service url service, that redirects according to links found in links.ini, or to a default url (such as search engine) for shortcuts not found
-// based on Martin Angelov script at tutorialzine.com
+	
+// original based on Martin Angelov script at tutorialzine.com
+// 2nd generation based on https://github.com/berteh/url-shortener	
+// This generation adds the ability to add links when they're not found
 
-//config
-define("REFRESHTIME", 4); //time (in seconds) to display a warning in case shortcut is not defined
-define("FIXURL", "http://www.google.com/search?q=");
-define("ALLOWLIST",1); //set to 0 to disable the /?list=1 url to list all shortcuts available
-
-//usually nothing to configure below.
-$start = microtime(true);
-
-$links = parse_ini_file('links.ini');
-$short = trim($_SERVER['REQUEST_URI'], '/');
+/* structure of document:
+	1. HANDLE SUBMISSIONS
+	2. TRY TO LOAD URL
+	3. URL NOT FOUND, ASK TO ADD
+*/
 
 
-if(array_key_exists($short, $links)){
-    header("Location: ".$links[$short]);
+// 1. HANDLE SUBMISSIONS
+if($_SERVER['REQUEST_METHOD'] == 'POST')
+{
+	$newshortcut = strtolower($_REQUEST["newshortcut"]); // lowercase
+	$newurl = $_REQUEST["newurl"]; // grab the url
+	$newline = "$newshortcut = \"$newurl\"\n"; // set up the line	
+	file_put_contents("links.ini", $newline, FILE_APPEND); // write it
+	
+	// todo: add confirmation
+	
+	echo "<a href='$newurl'>Go to $newurl?</a>"; // once it's submitted, provide the link		
+	?>
+		<h2>Add new thing</h2>
+		<form method="post" action="index.php">
+			<input type="text" name="newshortcut">
+			<input type="text" name="newurl">
+			<input type="submit">
+		</form>
+		<?php
+
+
+// 2. TRY TO LOAD URL
+}else{
+
+
+	$url=$_SERVER['REQUEST_URI']; // get the url
+	$shortcut = basename($url); // take the last thing in this URL and pass it along
+	$links = parse_ini_file('links.ini'); // look for that reference in the ini file
+	
+	// let's make sure the search is case insensitive
+	array_change_key_case($links);
+	
+	if(array_key_exists($shortcut, $links)){
+	    header("Location: ".$links[$shortcut]);
+	}else{ // 3. URL NOT FOUND, ASK TO ADD
+		?>
+		
+		<h2>Want to add this?</h2>
+		
+		<form method="post" action="index.php">
+			<input type="text" name="newshortcut" value="<?php echo $shortcut ?>">
+			<input type="text" name="newurl">
+			<input type="submit">
+		</form>
+		
+		<?php
+	}
+
 }
-elseif(ALLOWLIST && isset($_GET["list"])){
-    $end = microtime(true);
-    $time = round($end - $start, 7) . " sec";
-    $title = "Shortcuts on ".$_SERVER['HTTP_HOST'];
 
-    echo <<<EOT
-<html><head>
-<title>$title</title>
-<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/sortable/0.6.0/css/sortable-theme-finder.css">
-<style>body{font-family: sans-serif; font-size: 12px;}</style>
-<script src="//cdnjs.cloudflare.com/ajax/libs/sortable/0.6.0/js/sortable.min.js"></script>
-</head><body>
-
-<h2>$title</h2>
-<p>Loading the list of links took $time.</p>
-<table data-sortable class="sortable-theme-finder">
-<thead><tr><th>#</th><th>short</th><th>long</th></thead><tbody>
-EOT;
-    $i = 1;
-    foreach ($links as $s => $l) {
-	echo "<tr> <td>".$i++."</td><td><a href='http://".$_SERVER['HTTP_HOST']."/$s'>$s</a></td> <td><a href='$l'>".substr($l,0,70)."</a></td> </tr>\n ";
-    }
-    echo "</tbody></table>\n</body></html>";
-}
-else{
-    $fix = FIXURL."$short";
-    header('HTTP/1.0 404 Not Found');
-    header("Refresh: REFRESHTIME; URL=$fix");
-    echo "<p>Unknown shortcut <em>$short</em>, you will shortly be redirected to our default <a href='$fix'>search page</a>, we hope it helps!<p>";
-    error_log("'$short' is not an existing shortcut\n", 3, dirname(__FILE__)."/url-shortener-missing.log");
-//    error_log("'$short' is not an existing shortcut, add it to links.ini if needed", 1, "you@mail.org", "Subject: [error] shortcut at ".$_SERVER['HTTP_HOST']); //get a warning by mail
-}
+?>
